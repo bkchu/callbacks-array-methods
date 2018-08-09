510e4f239173bb2f6bb08e72cf56c353b4d5e980
Brandon Chung <brandon.chung@devmounta.in>
2018-08-09 16:41:50 -0500

    finished implenting alternate array methods

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
-/* includes */
+const arr = ["derp", 1, 'poop', {obj: "nice"}];
 
-/* indexOf */
+Array.prototype.altIncludes = function(item) {
+  return this.indexOf(item) !== -1;
+}
 
-/* forEach */
+arr.altIncludes("derp");
 
-/* map */
+Array.prototype.altIndexOf = function(item) {
+  let index = -1;
+  for(let i = 0; i < this.length; i++) {
+    if(item === this[i]) {
+      index = i;
+    }
+  }
+  return index; 
+}
 
-/* filter */
+arr.altIndexOf('poopd');
+arr.altIndexOf('poop');
 
-/* reduce */
+Array.prototype.altForEach = function(cb) {
+  for(let i = 0; i < this.length; i++) {
+    this[i] = cb(this[i], i, this);
+  }
+  return this;
+}
+
+arr.altForEach(thing => thing + "hello");
+
+Array.prototype.altMap = function(cb) {
+  let arr = [];
+  for(let i = 0; i < this.length; i++) {
+    arr.push(cb(this[i], i, this));
+  }
+  return arr;
+}
+
+const mappedArr = arr.altMap(item => item + "Hello");
+console.log({mappedArr});
+
+Array.prototype.altFilter = function(cb) {
+  let arr = [];
+  for(let i = 0; i < this.length; i++) {
+    if(cb(this[i], i, this)) {
+      arr.push(this[i]);
+    }
+  }
+  return arr;
+}
+
+const filteredArr = arr.altFilter(item => typeof item === "string");
+console.log({filteredArr});
+
+Array.prototype.altReduce = function(cb, acc = 0) {
+  for(let i = 0; i < this.length; i++) {
+    acc = cb(acc, this[i], i, this)
+  }
+  return acc;
+}
+
+const arr2 = [['a', 'k', 'b'],['d','F',' '],['g', 'H', 'X']];
+function sorter(arr) {
+  return arr.altReduce((acc, cur) => ([...acc, ...cur])).sort();
+}
+
+sorter(arr2);
